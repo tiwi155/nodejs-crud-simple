0c708c6b2f1bc65d326b7cd8df282a23119ca546
Sindu Aji <ajisindu107@gmail.com>
2025-09-04 11:00:44 +0700

    pesan commit pertama

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 // routes/userRoutes.js
-const express = require('express');
+const express = require ('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
